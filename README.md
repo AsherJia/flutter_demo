a80ed32cd163c91a07989823a6e06133f1b3f874
jiazw <jiazw@ctrip.com>
2019-07-10 19:31:14 +0800

    day 1

--- README.md
+++ README.md
@@ -1,16 +1,71 @@
-# flutter_demo
-
-A new Flutter project.
-
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+[Flutter SDK](https://api.flutter.dev/)
 
-A few resources to get you started if this is your first Flutter project:
+## Widget
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- Accessibility
+- Animation and Motion
+- Assets, Images, and Icons
+- Async
+- Basics
+- Cupertino (iOS-style widgets)
+- Input
+- Interaction Models
+- Layout
+- Material Components
+- Painting and effects
+- Scrolling
+- Styling
+- Text
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+## StatefulWidget lifecycle
+
+![StatefulWidget](./doc/pic/StatefulWidgetLifecycle.png)
+
+- StatefulWidget
+    - createState
+- State
+    - mounted is true
+      - mounted 为 true 的时候才能使用setState() ??? 设置为false也可以用
+    - initState
+    - didChangeDependencies
+      - Called when a dependency of this element changes.
+    - build
+    - deactivate
+    - dispose
+    - mounted is false
+
+    - didUpdateWidget
+      - check the runtimeType and Widget.key
+
+## StatelessWidget lifecycle
+
+- build
+
+## MaterialApp
+
+MaterialApp 表示当前应用的风格是 Material Design。为了使用其他已经封装好的 Material Design 风格的 Widget，就必须使用 MaterialApp。因此 MaterialApp 经常是 Flutter Widget 树里的第一个元素，就是 Root Widget。
+
+- title
+  - 这个 title 是出现在 Android 的任务管理器上或者是 iOS 程序切换管理器中
+- color
+- theme
+  - ThemeData 类型，比如颜色，字体
+- home
+  - widget 类型，进入程序后展示的第一个页面
+
+
+## Scaffold
+
+Scaffold 实现了 Material Design 的基本布局结构，例如 AppBar、Drawer、SnackBar 等，所以为了使用这些布局，也必须要使用 Scaffold，所以一个 Flutter App 的 基本结构就是：Root Widget 是 MaterialApp ，然后 MaterialApp 的 子Widget 就是 Scaffold，然后我们在 Scaffolfd 的 子Widget 里写UI。
+
+> 实现`Material Design`基本视觉不觉结构的widget，它被设计为Material App的顶级容器，会自动填满屏幕，适配刘海屏。
+
+- appBar
+  - 顶部的标题栏，不设置的话就不会显示。
+- backgroundColor
+  - Color类型
+- body
+  - Widget类型
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,170 +1,284 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp('Hello World11111'));
+
+// 声明文本样式
+const textStyle = const TextStyle(
+  fontFamily: 'OperatorMono',
+);
+
+// 使用文本样式
+var buttonText = const Text(
+  "const Use the font for this text1234567890",
+  style: textStyle,
+);
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final String content;
+
+  MyApp(this.content);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test Demo',
+      title: 'Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue
       ),
-      home: CounterWidget(),
-      //注册路由表
-      routes: {
-        "page1": (context) => Page1(),
-        "page2": (context) => Echo(text: '...')
-      } ,
-    );
-  }
-}
-
-class Page1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-        title: new Text('Page1'),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text('This is page11.')
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Demo'),
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: () {
-          print('Click page1 button.');
-          Navigator.pushNamed(context, "page2");
-        },
-        tooltip: 'Increment',
-        child: new Icon(Icons.add)
+        body: Center(
+          child: Column(
+            children: [
+              buttonText,
+              Text("const Use the font for this text1234567890")
+            ],
+          ),
+        )
       )
     );
   }
 }
 
-class Page2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-        title: new Text('Page2'),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text('This is page2.')
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: () {
-          print('Click page2 button.');
-          Navigator.pushNamed(context, "page1");
-        },
-        tooltip: 'Increment',
-        child: new Icon(Icons.add)
-      )
-    );
-  }
-}
+// class MyApp extends StatefulWidget {
+//   String content;
 
-class Echo extends StatelessWidget {
-  const Echo({
-    Key key,
-    @required this.text,
-    this.backgroundColor: Colors.grey
-  }): super(key: key);
+//   MyApp(this.content);
 
-  final String text;
-  final Color backgroundColor;
+//   @override
+//   State<StatefulWidget> createState() {
+//     return MyAppState();
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: backgroundColor,
-        child: Text(text),
-      ),
-    );
-  }
-}
+// class MyAppState extends State<MyApp> {
+//   bool isShowText = true;
 
-class CounterWidget extends StatefulWidget {
-  const CounterWidget({
-    Key key,
-    this.initValue: 0
-  });
+//   void increment() {
+//     print('setState...');
+//     setState(() {
+//       widget.content += "d";
+//     });
+//   }
 
-  final int initValue;
+//   @override
+//   void initState() {
+//     super.initState();
+//     print('initState...');
+//   }
 
-  @override
-  State<StatefulWidget> createState() => new _CounterWidgetState();
-}
+//   @override
+//   void didChangeDependencies() {
+//     super.didChangeDependencies();
+//     print('didChangeDependencies...');
+//   }
 
-class _CounterWidgetState extends State<CounterWidget> {
-  int _counter;
-  // flutter: initState...
-  // flutter: didChangeDependencies....
-  // flutter: builder...
-  // flutter: reassemble....
-  // flutter: didUpdateWidget....
-  // flutter: builder...
+//   @override
+//   Widget build(BuildContext context) {
+//     print('build...');
+//     return MaterialApp(
+//         title: 'Test',
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//         ),
+//         home: Scaffold(
+//           appBar: AppBar(title: Text("Widget -- StatefulWidget and state")),
+//           body: Center(
+//             child: GestureDetector(
+//                 child: isShowText ? Text(widget.content) : null,
+//                 onTap: increment),
+//           ),
+//         ));
+//   }
 
-  @override
-  void initState() {
-    super.initState();
+//   @override
+//   void deactivate() {
+//     super.deactivate();
+//     print('deactivate...');
+//   }
 
-    _counter = widget.initValue;
-    print('initState...');
-  }
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     print('dispose...');
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    print('builder...');
-    return Scaffold(
-      body: Center(
-        child: FlatButton(
-          child: Text('$_counter'),
-          onPressed: () => setState(() => ++_counter),
-        ),
-      ),
-    );
-  }
+//   @override
+//   void didUpdateWidget(MyApp oldWidget) {
+//     super.didUpdateWidget(oldWidget);
+//     print('didUpdateWidget...');
+//   }
+// }
 
-  @override
-  void didUpdateWidget(CounterWidget oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print("didUpdateWidget....");
-  }
+// import 'package:flutter/material.dart';
 
-  @override
-  void deactivate() {
-    super.deactivate();
-    print("deactivate....");
-  }
+// void main() => runApp(MyApp());
 
-  @override
-  void dispose() {
-    super.dispose();
-    print("dispost....");
-  }
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Test Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: CounterWidget(),
+//       //注册路由表
+//       routes: {
+//         "page1": (context) => Page1(),
+//         "page2": (context) => Echo(text: '...')
+//       } ,
+//     );
+//   }
+// }
 
-  @override
-  void reassemble() {
-    super.reassemble();
-    print('reassemble....');
-  }
+// class Page1 extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: new AppBar(
+//         title: new Text('Page1'),
+//       ),
+//       body: new Center(
+//         child: new Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             new Text('This is page11.')
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: new FloatingActionButton(
+//         onPressed: () {
+//           print('Click page1 button.');
+//           Navigator.pushNamed(context, "page2");
+//         },
+//         tooltip: 'Increment',
+//         child: new Icon(Icons.add)
+//       )
+//     );
+//   }
+// }
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print('didChangeDependencies....');
-  }
-}
+// class Page2 extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: new AppBar(
+//         title: new Text('Page2'),
+//       ),
+//       body: new Center(
+//         child: new Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             new Text('This is page2.')
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: new FloatingActionButton(
+//         onPressed: () {
+//           print('Click page2 button.');
+//           Navigator.pushNamed(context, "page1");
+//         },
+//         tooltip: 'Increment',
+//         child: new Icon(Icons.add)
+//       )
+//     );
+//   }
+// }
+
+// class Echo extends StatelessWidget {
+//   const Echo({
+//     Key key,
+//     @required this.text,
+//     this.backgroundColor: Colors.grey
+//   }): super(key: key);
+
+//   final String text;
+//   final Color backgroundColor;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Container(
+//         color: backgroundColor,
+//         child: Text(text),
+//       ),
+//     );
+//   }
+// }
+
+// class CounterWidget extends StatefulWidget {
+//   const CounterWidget({
+//     Key key,
+//     this.initValue: 0
+//   });
+
+//   final int initValue;
+
+//   @override
+//   State<StatefulWidget> createState() => new _CounterWidgetState();
+// }
+
+// class _CounterWidgetState extends State<CounterWidget> {
+//   int _counter;
+//   // flutter: initState...
+//   // flutter: didChangeDependencies....
+//   // flutter: builder...
+//   // flutter: reassemble....
+//   // flutter: didUpdateWidget....
+//   // flutter: builder...
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     _counter = widget.initValue;
+//     print('initState...');
+//   }
+
+//   @override
+//   void didChangeDependencies() {
+//     super.didChangeDependencies();
+//     print('didChangeDependencies....');
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     print('builder...');
+//     return Scaffold(
+//       body: Center(
+//         child: FlatButton(
+//           child: Text('$_counter'),
+//           onPressed: () => setState(() => ++_counter),
+//         ),
+//       ),
+//     );
+//   }
+
+//   @override
+//   void reassemble() {
+//     super.reassemble();
+//     print('reassemble....');
+//   }
+
+//   @override
+//   void didUpdateWidget(CounterWidget oldWidget) {
+//     super.didUpdateWidget(oldWidget);
+//     print("didUpdateWidget....");
+//   }
+
+//   @override
+//   void deactivate() {
+//     super.deactivate();
+//     print("deactivate....");
+//   }
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     print("dispost....");
+//   }
+// }
